07bd1a637bddda05921d34df2af60f3871976e7f
potochii <621413017@crru.ac.th>
2023-11-20 12:11:39 +0700

    แก้ bug
    แก code2

--- app.js
+++ app.js
@@ -3,18 +3,25 @@ const express =require('express');
 const debug = require('debug')('app');
 const morgan = require('morgan');
 
+const path = require('path');
+
 const app= express();
 const port = 3000;
 
-app.use(morgan('combined'));
+//app.use(express.static(path.join(__dirname,"/public/")));
 
+//app.set("views","./src/views");
+//app.set("view emgine","ejs");
+
+app.use(morgan('combined'));
+app.use(express.static(path.join(__dirname,"/public/")));
 
 app.get("/",(req,res) =>{ 
 
 
 
-res.send('Hello sawasdekaaaaa');
-
+//res.send('Hello sawasdekaaaaa');
+res.render('index',{username:'potochii',customer:["eiei","koko","riri"]});
 
 })
 
