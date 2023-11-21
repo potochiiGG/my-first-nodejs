6b32c2afda2962bae7cbcd3504be83ec2b08bf57
potochii <621413017@crru.ac.th>
2023-11-21 10:28:30 +0700

    update

--- app.js
+++ app.js
@@ -5,8 +5,10 @@ const morgan = require('morgan');
 
 const path = require('path');
 
+const productRouter = express.Router();
+
 const app= express();
-const port = 3000;
+const PORT = process.env.PORT || 4000;
 
 //app.use(express.static(path.join(__dirname,"/public/")));
 
@@ -16,19 +18,41 @@ const port = 3000;
 app.use(morgan('combined'));
 app.use(express.static(path.join(__dirname,"/public/")));
 
+app.set("views","./src/views");
+app.set("view engine","ejs")
+
+productRouter.route("/").get((req, res)=> {
+  
+  res.send('Hello sawasdekaaaaa');
+
+});
+
+productRouter.route("/2").get((req, res)=> {
+  
+  res.send('Hello sawasdekaaaaa22222');
+
+});
+
+
+
+app.use("/products", productRouter)
+
 app.get("/",(req,res) =>{ 
 
 
-
+//res.render('index',{username:'potochii'});
 //res.send('Hello sawasdekaaaaa');
 res.render('index',{username:'potochii',customer:["eiei","koko","riri"]});
+
+
+
 
 })
 
 
 
 
-app.listen(port,() =>{
+app.listen(PORT,() =>{
 
     import('chalk').then(module => {
     const chalk = module.default || module;
@@ -37,7 +61,7 @@ app.listen(port,() =>{
     
     //console.log("you coming start on port : "+chalk.blue(port));
 
-    debug("you coming start on port : "+chalk.blue(port));
+    debug("you coming start on port : "+chalk.blue(PORT));
 
     //console.log(chalk.red.bold('This is a bold red text.'));
   })
